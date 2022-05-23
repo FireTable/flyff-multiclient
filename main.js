dc8879d00df4e90d091e8fb10d7df9db77dd2337
Yongzhuo Liang <firetable@foxmail.com>
2022-05-23 18:47:02 +0800

    temp: go home continue

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, protocol } = require('electron')
+
+const { session } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 2560 / 2,
-        height: 1440 / 2
+        height: 1440 / 2,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+            webSecurity: false, // 允许跨域
+            allowRunningInsecureContent: false,
+        }
     })
 
     win.loadURL('http://universe.flyff.com/play')
+
+    // 打开开发者工具
+    // win.webContents.openDevTools()
 }
 
+
 app.whenReady().then(() => {
+
+    const webReqFilter = {
+        urls: ['*://*/*']
+    }
+
+    const isLanguageRedirect = (request) => {
+        var fPath = request.url;
+        if (fPath.indexOf('/language') >= 0) {
+            return true
+        }
+        return false
+    }
+
+    session.defaultSession.webRequest.onBeforeRequest(webReqFilter, (request, callback) => {
+        if (isLanguageRedirect(request)) {
+            const redirectURL = request.url.replace('/language/en', '/language/cns')
+            callback({ redirectURL, cancel: false });
+            return
+        }
+        callback({});
+    });
+
+
     createWindow()
 })
